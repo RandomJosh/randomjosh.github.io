7f8759da40f496f953c05b68504d72e8d5fa014a
RandomJosh <randomjosh@outlook.com>
2017-05-18 13:18:18 +0100

    Update made from feedback

--- TestJava.js
+++ TestJava.js
@@ -10,53 +10,33 @@ function changeFontSize(size) {
 	localStorage.setItem("fontSize", size);
 }
 
-/*function changeBackgroundColour(colour) {
-	var fontColour = document.body.style.color.text;
-
-	if (fontColour == colour){
-		alert("Cannot choose same colour as the font");
-	}
-	else {
-		document.body.style.backgroundColor = colour;
-		localStorage.setItem("backgroundColour", colour);
-	}
-
-}*/
+//changed the background colour upon selection
 
 function changeBackgroundColour() {
 	var eID = document.getElementById("bgColour");
 	var colourtxt = eID.options[eID.selectedIndex].text;
 	if (colourtxt == localStorage.getItem("fontColour")){
-		alert("Cannot choose the same colour as the font! Please select another colour");
+		alert("Cannot choose the same colour as the font! Please select another colour"); //alerts the user they cannot choose the same colour to avoid colour clash
 	}else {
 	document.body.style.backgroundColor = colourtxt;
-	localStorage.setItem("backgroundColour", colourtxt);
+	localStorage.setItem("backgroundColour", colourtxt); //sets the local storage for backgroundColour
 	}
 }
 
-
-/*function changeFontColour(colour){
-	var backgroundColour = document.body.style.backgroundColor.text;
-
-	if (backgroundColour == colour){
-		alert("Cannot choose same colour as the background");
-	}
-	else {
-		document.body.style.color = colour;
-		localStorage.setItem("fontColour", colour);
-	}
-}*/
+//changes the font colour upon selection
 
 function changeFontColour() {
 	var eID = document.getElementById("fontColour");
 	var colourtxt = eID.options[eID.selectedIndex].text;
 	if (colourtxt == localStorage.getItem("backgroundColour")){
-		alert("Cannot choose the same colour as the background! Please select another colour");
+		alert("Cannot choose the same colour as the background! Please select another colour"); //alerts the user they cannot choose the same colour to avoid colour clash
 	}else {
 	document.body.style.color = colourtxt;
-	localStorage.setItem("fontColour", colourtxt);
+	localStorage.setItem("fontColour", colourtxt); //sets the local storage for fontColour
 	}
 }
+
+//Resets the styling options upon pressing the reset button and also resets the local storage data so it is not confused
 
 function resetStyle(){
 	var backgroundColour = "cornflowerblue";
@@ -65,7 +45,11 @@ function resetStyle(){
 	document.body.style.color = fontColour;
 	localStorage.setItem("backgroundColour", backgroundColour);
 	localStorage.setItem("fontColour", fontColour);
+	document.getElementById('bgColour').value=0;
+	document.getElementById('fontColour').value=0;
 }
+
+//checks the styling options upon loading the page
 
 function checkStyles(){
 	document.body.style.backgroundColor = localStorage.getItem("backgroundColour");
@@ -74,27 +58,22 @@ function checkStyles(){
 	
 }
 
-function TestFunction() {
-
-	alert("Hello!!");
-
-}
-
 //Verification checks for contact form
+//checks to ensure that the name meets the set criteria
 
 function checkNameInput() {
 	var name = document.getElementById("InputName").value;
 	var letters = /^[A-Za-z ]+$/;
 
-	if (name == ""){
+	if (name == ""){ //checks if its blank
 		document.getElementById("nameAlertField").innerHTML = "Please enter your name";
 		return false;
 	}
-	else if (name.length < 3) {
+	else if (name.length < 3) { //checks if it is below 3 characters
 		document.getElementById("nameAlertField").innerHTML = "Please enter a valid name";
 		return false;
 	}
-	else if (letters.test(name) == false) {
+	else if (letters.test(name) == false) { //checks if it is only alphabetical characters used
 		document.getElementById("nameAlertField").innerHTML = "Please enter a valid name";
 		return false;
 	}
@@ -105,15 +84,17 @@ function checkNameInput() {
 
 }
 
+//checks to ensure the email meets the set criteria
+
 function checkEmailInput() {
 	var emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 	var email = document.getElementById("InputEmail").value;
 
-	if (email == "") {
+	if (email == "") { //checks if its blank
 		document.getElementById("emailAlertField").innerHTML = "Please enter your email";
 		return false;
 	}
-	else if (emailPattern.test(email) == false) {
+	else if (emailPattern.test(email) == false) { //checks if it matches the email pattern
 		document.getElementById("emailAlertField").innerHTML = "Please enter a valid email address";
 		return false;
 	}
@@ -124,13 +105,15 @@ function checkEmailInput() {
 	}
 }
 
+//Checks the message to ensure that it meets the set criteria
+
 function checkMessageInput() {
 	var message = document.getElementById("InputMessage").value;
-	if (message == ""){
+	if (message == ""){ //checks if its blank
 		document.getElementById("messageAlertField").innerHTML = "Please enter a message";
 		return false;
 	}
-	else if (message.length < 50) {
+	else if (message.length < 50) { //checks the length of the message input
 		document.getElementById("messageAlertField").innerHTML = "Please enter a full message";
 		return false;
 	}
@@ -139,6 +122,8 @@ function checkMessageInput() {
 		return true;
 	}
 }
+
+//checks the above functions to see if they return a true and will either allow or not allow them to submit
 
 function buttonActivation() {
 	if (checkMessageInput() && checkEmailInput() && checkNameInput()) {
@@ -149,6 +134,8 @@ function buttonActivation() {
 		return false;
 	}
 }
+
+//function to be used within the "entry page" which adds a personalised greeting
 
 function nameInput() {
 	var Name = prompt("Please enter your name below")
@@ -175,9 +162,9 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
-
+//Changes the navigation bar when screen width is of a certain size to allow for mobile support
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
